6eb8940dba8de253b5394b91cb366c5cd163c0b9
phucbm <hiphucbui@gmail.com>
2022-04-23 17:25:31 +0700

    config vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    pwa: {
+        'name': 'Todo MVC'
+    },
+    transpileDependencies: true
 })
