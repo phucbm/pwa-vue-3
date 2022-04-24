6128444d5f6863980e12dd9e5b44abb14f2bb9ff
phucbm <hiphucbui@gmail.com>
2022-04-24 12:12:41 +0700

    indexedDB: delete

--- src/App.vue
+++ src/App.vue
@@ -65,6 +65,25 @@ export default {
       todo.title = this.beforeEditCache
     },
 
+    async deleteTodo(todo){
+      this.database = await this.getDatabase();
+
+      return new Promise((resolve, reject) => {
+        const transaction = this.database.transaction("todos", 'readwrite')
+        const store = transaction.objectStore("todos")
+
+        store.delete(todo.id)
+
+        transaction.oncomplete = () => {
+          resolve('Successfully deleted!')
+        }
+
+        transaction.onerror = event => {
+          reject(event)
+        }
+      })
+    },
+
     doneEdit(todo){
       if(!this.editedTodo){
         return
@@ -147,13 +166,19 @@ export default {
 
     removeCompleted(){
       this.todos = this.todos.filter(item => {
-        return !item.completed
+        if(item.completed){
+          this.deleteTodo(item)
+        }else{
+          return !item.completed;
+        }
       })
     },
 
     removeTodo(todo){
       const index = this.todos.indexOf(todo)
       this.todos.splice(index, 1)
+
+      this.deleteTodo(todo)
     },
 
     async saveTodo(todo){
